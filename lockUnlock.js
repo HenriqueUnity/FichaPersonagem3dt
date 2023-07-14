327f007f553d7142ed9088e6f63496b82cc0fc38
Telmo <telmohenrique1@hotmail.com>
2023-07-14 03:21:48 -0300

    todo skills delete

--- lockUnlock.js
+++ lockUnlock.js
@@ -1,7 +1,16 @@
+let lock = false;
 function LockUnlock() {
-  let atributes = document.getElementsByClassName("atribute");
-  for (let index = 0; index < atributes.length; index++) {
-    atributes[index].disabled = !event.target.checked;
-    console.log(atributes[index]);
+  if (!lock) {
+    let atributes = document.getElementsByClassName("atribute");
+    for (let index = 0; index < atributes.length; index++) {
+      atributes[index].disabled = !event.target.checked;
+      lock = true;
+    }
+  } else {
+    let atributes = document.getElementsByClassName("atribute");
+    for (let index = 0; index < atributes.length; index++) {
+      atributes[index].disabled = event.target.checked;
+      lock = false;
+    }
   }
 }
